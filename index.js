263d81df12fdc6c9c7f9c48f5e7c5a57757ba2ce
EOMMINHO <djaalsgh159@kaist.ac.kr>
2020-11-10 21:29:27 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Http2ServerRequest } = require('http2');
+
+let app = require("express")();
+let http = require("http").createServer(app);
+let io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+io.on("connection", (socket) => {
+    console.log("a user connected");
+    socket.on("disconnect", ()=>{
+        console.log("user disconnected");
+    })
+    socket.emit("test1", 1, 2);
+})
+
+http.listen(3000, () => {
+    console.log("listening on port 3000");
+})
